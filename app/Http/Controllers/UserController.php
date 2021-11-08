a495176453fe03207e9e09d9b72f0af3fcc39e3b
uchchwas1 <uchchwas.das@northsouth.edu>
2021-11-08 13:04:17 +0600

    Create files for User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+//use Illuminate\Support\Facades\DB;
+use App\Models\Users;
 class UserController extends Controller
 {
    function login(){
-       return DB::select("select * from user");
+       //return DB::select("select * from user");
+       return Users::all();
    }
 }

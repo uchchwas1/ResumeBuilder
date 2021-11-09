0d012cac7b2da9e80c60f2d34f4c62e77b1b551a
uchchwas1 <uchchwas.das@northsouth.edu>
2021-11-09 13:22:02 +0600

    Create a migration table for User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\DB;
-use App\Models\Users;
+
 class UserController extends Controller
 {
-   function login(){
-       //return DB::select("select * from user");
-       return Users::all();
-   }
+   //
 }
---
+++ database/migrations/2021_11_09_065744_create_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_name');
+            $table->string('user_mobile');
+            $table->string('user_email');
+            $table->string('user_address');
+            $table->string('user_password');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}

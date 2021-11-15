064402a6599ac9809211ca7e559203e74a664330
uchchwas1 <uchchwas.das@northsouth.edu>
2021-11-15 13:51:30 +0600

    Fixed summary section
    in userDetailscontroller  in store function format to send data to summary

---
+++ app/Http/Controllers/UserDetailsController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserDetails;
+use Illuminate\Http\Request;
+
+class UserDetailsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $details = auth()->user()->details;
+        return view('usr_details.index', compact('details'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('usr_details.CreateNew');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // UserDetails::create($request->all());
+        // echo "Save Information Successfully";
+
+        // return back();
+
+        $request->validate([
+            'fullname' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'summary' => 'required'
+        ]);
+        $detail = new UserDetails();
+        
+        $detail->fullname = $request->input('fullname');
+        $detail->phone = $request->input('phone');
+        $detail->email = $request->input('email');
+        $detail->address = $request->input('address');
+        $detail->summary = $request->input('summary');
+        $detail->user_id = auth()->id();
+        $detail->save();
+
+        //return back();
+        return redirect()->route('education.details');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserDetails  $userDetails
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserDetails $userDetails)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserDetails  $userDetails
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserDetails  $userDetail)
+    {
+        return view('usr_details.edit', compact('userDetail'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserDetails  $userDetails
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserDetails $userDetail)
+    {
+        $request->validate([
+           'fullname' => 'required',
+           'email' => 'required|email',
+           'phone' => 'required',
+        ]);
+
+        $userDetail->update($request->except('_token'));
+        return redirect()->route('user-detail.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserDetails  $userDetails
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserDetails $userDetail)
+    {
+        $userDetail->delete();
+        return back();
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- <meta charset="utf-8"> -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <!-- JS Bundle -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <!-- CSS -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">   -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script> -->
+</head>
+<body id="app-layout">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/home">Resume Builder</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href=./home>Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href=" {{route('user-detail.index')}} ">Heading</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./login">Sign-in</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./registration">Sign-up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('education.index')}}">Education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('experience.index')}}">Work History</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('skill.index')}}">Skills</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  <main class = "py-4 container">
+    @yield('content')
+     
+  </main>  
+ <!--Model start--> 
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modelId">
+    Preview
+  </button>
+ 
+  <!-- Modal -->
+  <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Preview Resume</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+          <iframe src = "{{route('resume.index')}}" width = "100%" height = "900"></iframe>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <a name="" id="" class="btn btn-primary" href="{{route('resume.download')}}" role="button">Download</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--Model end-->   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
